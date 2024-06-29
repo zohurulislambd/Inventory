8bca7996611d84159c54e203a840b4b8c8dc3ad3
Md. Zohurul Islam <38900846+zohurulislambd@users.noreply.github.com>
2024-06-29 12:26:02 +0600

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using Inventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inventory.Controllers;
+using System.Web.UI.WebControls;
+namespace Inventory.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            ViewBag.Message = "";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Account Model)
+        {
+            //Double Layer validation
+            if (String.IsNullOrEmpty(Model.UserName) || String.IsNullOrEmpty(Model.Password))
+                ViewBag.Message = "Input Needed";
+
+
+            if (Model.UserName == "Zohurul" && Model.Password == "123")
+            {
+                Session["User"] = Model.UserName;
+                
+                ViewBag.Message = "Login Successfully";
+                //RedirectToAction("DashBoard", "Home");
+            }
+            else
+            { 
+                ViewBag.Message = "User or password is wrong";
+            }
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult DashBoard()
+        {
+            return View();
+        }
+    }
+}
